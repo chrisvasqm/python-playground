b4d717a0f1b56baecd0d76944a0f12893c7f4934
Christian Vasquez <christianvasquezmelo@gmail.com>
2018-11-24 09:07:01 -0400

    Refactor code

--- exercises/fizzbuzz.py
+++ exercises/fizzbuzz.py
@@ -1,19 +1,26 @@
+def main():
+    for number in range(1, 31):
+        if isFizzBuzz(number):
+            print("FizzBuzz")
+        elif isFizz(number):
+            print("Fizz")
+        elif isBuzz(number):
+            print("Buzz")
+        else:
+            print(number)
+
 
 def isFizz(number):
     return number % 3 == 0
 
+
 def isBuzz(number):
     return number % 5 == 0
+
 
 def isFizzBuzz(number):
     return number % 15 == 0
 
-for number in range(31):
-    if isFizzBuzz(number):
-        print("FizzBuzz")
-    elif isFizz(number):
-        print("Fizz")
-    elif isBuzz(number):
-        print("Buzz")
-    else:
-        print(number)
+
+if __name__ == "__main__":
+    main()
